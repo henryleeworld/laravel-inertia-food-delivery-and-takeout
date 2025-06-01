a530d7da2b5888ffe2a255ad0fca53e6378cc481
Henry Lee <henryleeworld@gmail.com>
2025-06-02 00:17:35 +0800

    feat:更新最新資料

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerGates();
+    }
+
+    protected function registerGates(): void
+    {
+        try {
+            foreach (Permission::pluck('name') as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return $user->hasPermission($permission);
+                });
+            }
+        } catch (\Exception $e) {
+            info('registerPermissions(): Database not found or not yet migrated. Ignoring user permissions while booting app.');
+        }
     }
 }
